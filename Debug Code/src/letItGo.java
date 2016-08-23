4762a5b19b855c06702d6fd3180b4635a26cd196
TestSheepNZ <mike_talks@hotmail.com>
2016-08-23 19:35:43 +1200

    Uploading

---
+++ Debug Code/src/letItGo.java
@@ -0,0 +1,51 @@
+/*
+ * This program is part of my learning Java series - specifically, to run through the debugger 
+ * to see what happens when each line is executed.
+ * 
+ * Find the blog here,
+ * http://testsheepnz.blogspot.co.nz/2016/08/java-1-do-you-want-to-build-program.html
+ * 
+ * For more information - please reread.
+ * 
+ * Mike Talks, Aug 2016
+ */
+
+public class letItGo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int line_count = 0;
+		
+		System.out.println("Let it go");
+		line_count++;
+
+		System.out.println("Let it go");
+		line_count++;
+
+		System.out.println("Can't hold it back anymore");
+		line_count++;
+
+		System.out.println("Let it go");
+		line_count++;
+
+		System.out.println("Let it go");
+		line_count++;
+
+		System.out.println("Turn away and slam the door");
+		line_count++;
+
+		System.out.println("I don't care");
+		line_count++;
+
+		System.out.println("What they're going to say");
+		line_count++;
+
+		System.out.println("Let the storm rage on");
+		line_count++;
+
+		System.out.println("The cold never bothered me anyway");
+		line_count++;		
+
+	}
+
+}
